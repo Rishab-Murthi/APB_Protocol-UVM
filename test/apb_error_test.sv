1db892a9c15c11caae29072e369e174a3a7c59f3
Rishab-Murthi <rishabofficial057@gmail.com>
2026-01-22 16:11:13 +0530

    Add apb_error_test class for error testing

---
+++ test/apb_error_test.sv
@@ -0,0 +1,18 @@
+`ifndef APB_ERROR_TEST
+`define APB_ERROR_TEST
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+class apb_error_test extends apb_base_test;
+  `uvm_component_utils(apb_error_test)
+  function new(string name = "apb_error_test", uvm_component parent);
+    super.new(name,parent);
+  endfunction
+  task run_phase(uvm_phase phase);
+    phase.raise_objection(this);
+    env.agt.seq.opt = err;
+    env.agt.seq.count = 10;
+    env.agt.seq.start(env.agt.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
+`endif
